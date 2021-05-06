f57cd1c07d3254b8375ba4f6ad5fc2ba7a065618
Juuso Luoto <luotoju@rsd407.org>
2021-05-06 12:49:35 -0700

    score

--- Base project/MyGame/MyGame/GameScene.h
+++ Base project/MyGame/MyGame/GameScene.h
@@ -5,6 +5,11 @@
 class GameScene : public Scene {
  public:
   GameScene();
+
+  int getScore();
+  void increaseScore();
+private:
+	int score_ = 0;
 };
 
 typedef std::shared_ptr<GameScene> GameScenePtr;
